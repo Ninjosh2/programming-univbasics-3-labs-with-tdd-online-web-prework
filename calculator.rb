0cb9e077cca253a6571d3cb8a003c1276356f218
Joshua Sanders <ninjosh2@gmail.com>
2019-12-08 22:46:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,10 @@
 first_number = (20 + 20)
+
 second_number = (5 + 5)
+
 sum = (first_number + second_number)
+
 difference = (first_number - second_number)
+
+product = (first_number * second_number)
 
